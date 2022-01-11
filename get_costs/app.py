4fd64c173e4528081908da42f370a5a0cee99c39
Mike Duffy <mike.duffy@parivedasolutions.com>
2022-01-11 12:30:55 -0600

    Disable layer for now. Too many issues.

--- get_costs/app.py
+++ get_costs/app.py
@@ -1,12 +1,13 @@
 import os
-
-from project_blue_costs import get_cost_report
+from project_blue_costs import get_cost_report, post_to_slack
 
 
 def lambda_handler(event, context):
     tenants_role_arn = os.environ['TENANTS_ROLE_ARN']
     print('TENANTS_ROLE_ARN:', tenants_role_arn)
     cost_report = get_cost_report(tenants_role_arn, region='us-west-2')
+    post_to_slack(cost_report)
+
     return {
         "statusCode": 200,
         "body": cost_report
